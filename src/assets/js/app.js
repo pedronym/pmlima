9c3227a78305575000ad793b1ccdef87ac38e5b5
Pedro Lima <pedro.nym@gmail.com>
2016-07-27 01:25:48 +0100

    Running 100% on the smallest form factor (in this case the iPhone 4 screen with 320px of width

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+
+(function(win, doc, undefined){
+    $(document).ready(function(){
+
+        var introHugeText = doc.querySelector('.intro-huge');
+
+        // var textAnimation = window.setInterval(function() {
+        //     if (introHugeText.classList.contains('highlight')) {
+        //         introHugeText.classList.remove('highlight');
+        //     } else {
+        //         introHugeText.classList.add('highlight');
+        //     }
+        // }, 5000);
+    })
+})(window, document, undefined);
