1b3ce8fe35803190d38a68bc51d307626acf8a4e
Pedro Lima <pedro.nym@gmail.com>
2016-08-06 18:36:14 +0100

    More tweaks added to V2. Will start to outline how to create a Projects section.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,18 @@
-$(document).foundation();
+'use strict';
+
+(function (win, doc) {
+  console.log('App Running');
+
+  function onScroll (ev) {
+    var scrollY = $(body).scrollTop();
+
+    scrollY > fixedY ? $(header).addClass('fixed') : $(header).removeClass('fixed') ;
+  }
+
+  var body   = $(doc.body),
+      header = $('header'),
+      fixedY = 200;
+
+  $(win).on('scroll', onScroll);
+
+})(window, document)
